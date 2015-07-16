6f75b91ffbcdbede2db474d18c0401fa9c97b19e
britneywright <blw06g@gmail.com>
2015-07-16 11:57:51 -0400

    make seed data

--- bees-knees-news/db/seeds.rb
+++ bees-knees-news/db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+about = [Faker::Lorem.sentence, "", Faker::Company.catch_phrase]
+10.times { User.create(username: Faker::Internet.user_name, password: "peanuts", about: about.sample) }
+30.times { Post.create(title: Faker::Lorem.sentence(3), link: Faker::Internet.url, user_id: User.all.sample.id) }
+50.times {Comment.create(body: Faker::Lorem.sentence(10), user_id: User.all.sample.id, post_id: Post.all.sample.id) }
