c323e596bf76ecc38d0bc70fc8444fab3efdbba9
britneywright <blw06g@gmail.com>
2015-07-16 11:32:42 -0400

    add associations and validations to user, comment, and post models

--- bees-knees-news/app/models/post.rb
+++ bees-knees-news/app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+  has_many :comments
+  belongs_to :user
+
+  validates :title, :link, presence: true
+  validates :link, uniqueness: true
 end
--- bees-knees-news/app/models/user.rb
+++ bees-knees-news/app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+  has_many :comments
+  has_many :posts
+  has_secure_password
+
+  validates :username, presence true
 end
