0f568f2eafb5d620fe30ce45445c196b7d1995f0
britneywright <blw06g@gmail.com>
2015-07-27 12:30:03 -0400

    test user logs in

--- bees-knees-news/spec/features/users_spec.rb
+++ bees-knees-news/spec/features/users_spec.rb
@@ -6,7 +6,19 @@ feature 'Sign Up' do
     click_link "login"
     find(".signup_form").fill_in "Username", with: "janedog"
     find(".signup_form").fill_in "Password", with: "janedog"
-    click_button "Sign Up"
     expect(page).to have_content "janedog"
+  end
+end
+
+feature 'Log In' do
+  let!(:user) {User.create(username: 'britney', password: 'britney')}
+
+  scenario "user logs in" do
+    visit "/"
+    click_link "login"
+    find(".login_form").fill_in "Username", with: user.username
+    find(".login_form").fill_in "Password", with: user.password
+    click_button "Login"
+    expect(page).to have_content user.username
   end
 end
