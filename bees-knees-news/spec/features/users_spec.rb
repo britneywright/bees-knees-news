d06eed548940daf03849f3440f95b4e8a80c33d3
britneywright <blw06g@gmail.com>
2015-07-27 15:44:31 -0400

    test viewing user profiles

--- bees-knees-news/spec/features/users_spec.rb
+++ bees-knees-news/spec/features/users_spec.rb
@@ -1,24 +1,37 @@
 require 'rails_helper'
 
+
 feature 'Sign Up' do
+  let!(:user) {User.create(username: 'janedog', password: 'janedog')}
   scenario "user signs up" do
     visit "/"
     click_link "login"
-    find(".signup_form").fill_in "Username", with: "janedog"
-    find(".signup_form").fill_in "Password", with: "janedog"
+    find(".signup_form").fill_in "Username", with: user.username
+    find(".signup_form").fill_in "Password", with: user.password
+    click_button "Sign Up"
     expect(page).to have_content "janedog"
   end
 end
 
 feature 'Log In' do
   let!(:user) {User.create(username: 'britney', password: 'britney')}
-
   scenario "user logs in" do
-    visit "/"
-    click_link "login"
-    find(".login_form").fill_in "Username", with: user.username
-    find(".login_form").fill_in "Password", with: user.password
-    click_button "Login"
+    login user
     expect(page).to have_content user.username
+  end
+end
+
+feature 'Profile page' do
+  let!(:user) {User.create(username: 'britney', password: 'britney')}
+  scenario "own profile" do
+    login user
+    expect(page).to have_link "submissions"
+    expect(page).to have_link "comments"
+  end
+
+  scenario "other user's profile" do
+    login user
+    expect(page).to have_link "submissions"
+    expect(page).to have_link "comments"
   end
 end
